e9b1534475c9cc936ab26db5c054a678d03afe19
pizzaboy86 <160567751+pizzaboy86@users.noreply.github.com>
2025-05-22 09:51:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function start() {
   const startTime = performance.now()
   const runAmount = document.getElementById("input").value
-  var returnVal = ""
+  //var returnVal = ""
   for (i=0;i<runAmount;i++) {
     var num1 = Math.random()
     var num2 = Math.random()
     var result = num1 + num2
-    returnVal = returnVal + result + "\n"
+    //returnVal = returnVal + result + "\n"
   }
   const endTime = performance.now()
   const runTime = endTime-startTime
   document.getElementById("time").innerHTML = "Time ran: " + runTime + " ms"
-  document.getElementById("output").innerHTML = returnVal
+  //document.getElementById("output").innerHTML = returnVal
 }
