8e098fc5250465fc0b599bc499cb3f310f3d101f
pizzaboy86 <160567751+pizzaboy86@users.noreply.github.com>
2025-05-29 15:16:07 +0000

    garg

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
         Bodies = Matter.Bodies;
 
 var engine = Engine.create()
-
+var score = 0
 var render = Render.create({
   element : document.body,
   engine : engine,
@@ -23,11 +23,14 @@ var render = Render.create({
   }
 })
 
+var lostPoint = 0x0001,
+    gainPoint = 0x0002
+
 function randomInt(highest) {
   return Math.floor(Math.random() * highest)+1
 }
 
-var catcher = Bodies.rectangle(400,550,110,60, {isStatic:true, render: {fillStyle:"white", strokeStyle:"gray", lineWidth: 3}, label:"catcher", id: 1001})
+var catcher = Bodies.rectangle(400,550,200,20, {isStatic:true, render: {fillStyle:"white", strokeStyle:"gray", lineWidth: 3}, label:"catcher", id: 1001})
 var ground = Bodies.rectangle(400, 800, 5000, 20, {isStatic:true, render: {fillStyle:"red"}, label:"ground"})
 
 Composite.add(engine.world, [catcher, ground])
@@ -36,10 +39,14 @@ Events.on(engine, 'collisionStart', function(event) {
   var pairs = event.pairs.slice()
   var obj1 = pairs[0].bodyA
   var obj2 = pairs[0].bodyB
-    if (obj1.label == "catcher" || obj1.label == "ground") {
+    if (obj1.label == "catcher") {
       Composite.remove(engine.world, obj2)
-    } else if (obj2.label == "catcher" || obj2.label == "ground") {
+      score++
+      document.getElementById("score").innerHTML = "Score: " + score
+    } else if (obj2.label == "catcher") {
     Composite.remove(engine.world, obj1)
+    score++
+    document.getElementById("score").innerHTML = "Score: " + score
   }
 });
 
@@ -60,17 +67,15 @@ Events.on(engine, 'collisionStart', function(event) {
     // keep the mouse in sync with rendering
     render.mouse = mouse;
 
-let x = setInterval(() => {
+let spawner = setInterval(() => {
   var newfruit = Bodies.circle(randomInt(1000) + 200, 100, 30, {label:"fruit"})
   Composite.add(engine.world, newfruit)
 }, 250);
 
-let mouseInt = setInterval(() => {
-  var liveCatcher = Composite.get(catcher, 1001)
-  Body.setPosition(liveCatcher, mouse.position.x, liveCatcher.position.y)
-  document.getElementById("temp").innerHTML = mouse.position.x
-  document.getElementById("temp2").innerHTML = liveCatcher.position.y
-},10)
+let mover = setInterval(() => {
+  var liveCatcher = Composite.get(engine.world, 1001, "body")
+  Body.setPosition(liveCatcher, {x: mouse.position.x, y: 550})
+}, 10);
 
 Render.run(render)
 
